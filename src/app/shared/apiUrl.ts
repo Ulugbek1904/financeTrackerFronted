ac6dee0b0c43e047f441f23014336e979a3c9b86
Ulugbek <julugbek023@gmail.com>
2025-06-18 15:41:04 +0500

    fix

--- src/app/shared/apiUrl.ts
+++ src/app/shared/apiUrl.ts
@@ -5,7 +5,7 @@ export  class ApiUrls {
     accountUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api/accounts';
     categoryUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api/category';
     transactionUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api/transaction';
-    userUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api/home';
+    userUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api';
     authUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api';
     profileUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api/profile';
     budgetUrl: string = 'https://financetrackerbackend-1tp6.onrender.com/api/budget';
